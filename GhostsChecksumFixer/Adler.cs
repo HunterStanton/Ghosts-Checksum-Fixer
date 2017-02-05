e809a601bb04627b67fbe03e29ed81a5e9fbc17a
Hunter Stanton <ihatecompvir@icloud.com>
2017-02-05 05:48:42 -0500

    Initial commit

---
+++ GhostsChecksumFixer/Adler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+// Implements Adler32 checksumming, used in calculation of Ghosts savegame checksum
+
+namespace GhostsChecksumFixer
+{
+    class Adler
+    {
+        private static uint MOD_ADLER = 65521;
+        private uint Checksum;
+
+        public long Value
+        {
+            get
+            {
+                return (long)this.Checksum;
+            }
+        }
+
+        public Adler()
+        {
+            this.Reset();
+        }
+
+        public void Reset()
+        {
+            this.Checksum = 1U;
+        }
+
+        public void Update(byte[] buff)
+        {
+            this.Update(buff, buff.Length);
+        }
+
+        public void Update(byte[] buff, int length)
+        {
+            int offset = 0;
+
+            if (buff == null)
+            {
+                // Buffer should never be null, if is throw error
+                throw new ArgumentNullException("buf");
+            }
+
+            uint check1 = this.Checksum & (uint)ushort.MaxValue;
+            uint check2 = this.Checksum >> 16;
+            while (length > 0)
+            {
+                int check3 = 3800;
+
+                if (check3 > length)
+                {
+                    check3 = length;
+                }
+
+                length -= check3;
+
+                while (--check3 >= 0)
+                {
+                    check1 += (uint)buff[offset++] & (uint)byte.MaxValue;
+                    check2 += check1;
+                }
+
+                check1 %= Adler.MOD_ADLER;
+                check2 %= Adler.MOD_ADLER;
+            }
+            this.Checksum = check2 << 16 | check1;
+        }
+    }
+}
---
+++ GhostsChecksumFixer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GhostsChecksumFixer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                System.Console.WriteLine("GhostsChecksumFixer\nA tool for fixing the checksum in Ghosts savegames, so that they can be modified.\nWorks on PC, and would probably work on Xbox One and PS4.\nDoes NOT work on Xbox 360 or PS3 due to differing Endian.\nUsage: GhostsChecksumFixer <savegame.svg filename>");
+                return;
+            }
+            else
+            {
+                System.Console.WriteLine("GhostsChecksumFixer");
+                // Create a new filestream
+                FileStream savegameStream = new FileStream(args[0], FileMode.Open, FileAccess.ReadWrite);
+
+                // Create our binary reader/writer
+                BinaryReader reader = new BinaryReader((Stream)savegameStream);
+                BinaryWriter writer = new BinaryWriter((Stream)savegameStream);
+
+                // Get the original checksum from the file and store it
+                reader.BaseStream.Position = 0x8;
+                uint origChecksum = reader.ReadUInt32();
+
+                // Put the entire savegame after 0x500 (which is the data that is checksummed by the game) into a buffer
+                reader.BaseStream.Position = 0x500;
+                byte[] buffer = reader.ReadBytes((int)reader.BaseStream.Length - 0x500);
+                
+                // Calculate adler32 checksum of buffer
+                Adler adler32 = new Adler();
+                adler32.Update(buffer);
+
+                // Overwrite the adler32 sum that is stored in the savegame
+                writer.BaseStream.Position = 0x8;
+                writer.Write(adler32.Value);
+
+                // Flush and close our reader and writer
+                writer.Flush();
+                writer.Close();
+                reader.Close();
+
+                // Close the memory stream
+                savegameStream.Close();
+
+                // Print new checksum and original
+                System.Console.WriteLine("Savegame checksum updated!\nOriginal: " + origChecksum + " (" + origChecksum.ToString("X2")+")" + "\nNew: " + adler32.Value + " (" + adler32.Value.ToString("X2") + ")");
+
+                return;
+            }
+        }
+    }
+}
